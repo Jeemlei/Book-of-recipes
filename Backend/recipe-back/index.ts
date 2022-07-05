5fae033b1bd6c1cddfacd9767b437abce210a3df
Jeemlei <eemeli.ingervo@gmail.com>
2022-07-05 17:15:09 +0300

    Scripts for building the full application and serving built frontend through backend

--- Backend/recipe-back/index.ts
+++ Backend/recipe-back/index.ts
@@ -1,11 +1,15 @@
 import express from 'express'
+import path from 'path'
+
 const app = express()
+
 app.use(express.json())
+app.use(express.static(path.join(__dirname, 'static')))
 
 const PORT = 3000
 
-app.get('/', (_req, res) => {
-	res.send('hello')
+app.get('*', (_req, res) => {
+	res.sendFile(path.join(__dirname, 'static/index.html'))
 })
 
 app.listen(PORT, () => {
