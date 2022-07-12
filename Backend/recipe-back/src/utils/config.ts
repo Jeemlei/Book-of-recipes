c98513e85640a544d9fc1239d792e15f225ce402
Jeemlei <eemeli.ingervo@gmail.com>
2022-07-12 17:39:39 +0300

    MongoDB connection

--- Backend/recipe-back/src/utils/config.ts
+++ Backend/recipe-back/src/utils/config.ts
@@ -4,3 +4,11 @@ dotenv.config()
 export const PORT = process.env.PORT
 export const FRONTEND_PORT = process.env.FRONTEND_PORT
 export const NODE_ENV = process.env.NODE_ENV
+export const MONGODB_URI = process.env.MONGODB_URI
+
+export default {
+	PORT,
+	FRONTEND_PORT,
+	NODE_ENV,
+	MONGODB_URI
+}
