e1fa6eb9a24bc49569a78ff7f8521cf15264fec1
Jeemlei <eemeli.ingervo@gmail.com>
2022-07-13 08:00:40 +0300

    fixed log tests

--- Backend/recipe-back/tests/utils/logger.test.ts
+++ Backend/recipe-back/tests/utils/logger.test.ts
@@ -1,16 +1,12 @@
 import logger from '../../src/utils/logger'
 
-const consoleSpy = jest.spyOn(console, 'log').mockImplementation()
+jest.spyOn(console, 'log').mockImplementation()
+jest.spyOn(console, 'error').mockImplementation()
 
 describe('No logging during test runs', () => {
-	beforeEach(() => {
-		consoleSpy.mockClear()
-	})
-
 	test('Info log', () => {
-		const log = console.log
 		logger.info('test')
-		expect(log).not.toBeCalled()
+		expect(console.log).not.toBeCalled()
 	})
 
 	test('Error log', () => {
