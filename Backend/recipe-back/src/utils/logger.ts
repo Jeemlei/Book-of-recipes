3bcb396689ef764eb3bcc7da8082b761c023e656
Eemeli Ingervo <eemeli.ingervo@gmail.com>
2022-07-19 21:49:12 +0300

    ignore logging on code coverage

--- Backend/recipe-back/src/utils/logger.ts
+++ Backend/recipe-back/src/utils/logger.ts
@@ -2,12 +2,18 @@ import { NODE_ENV } from './config'
 
 const info = (msg: string) => {
 	if (NODE_ENV !== 'test') {
+		//There should be no logging during test runs
+		//so error logging is ignored from code coverage
+		/* istanbul ignore next */
 		console.log(msg)
 	}
 }
 
 const error = (msg: string) => {
 	if (NODE_ENV !== 'test') {
+		//There should be no logging during test runs
+		//so error logging is ignored from code coverage
+		/* istanbul ignore next */
 		console.error(msg)
 	}
 }
