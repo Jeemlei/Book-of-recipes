86b29df8785322b9567dd892267e4483ae3e1985
Eemeli Ingervo <eemeli.ingervo@gmail.com>
2022-07-19 21:56:25 +0300

    Update logger.ts

--- Backend/recipe-back/src/utils/logger.ts
+++ Backend/recipe-back/src/utils/logger.ts
@@ -1,19 +1,19 @@
 import { NODE_ENV } from './config'
 
 const info = (msg: string) => {
+	//There should be no logging during test runs
+	//so error logging is ignored from code coverage
+	/* istanbul ignore next */
 	if (NODE_ENV !== 'test') {
-		//There should be no logging during test runs
-		//so error logging is ignored from code coverage
-		/* istanbul ignore next */
 		console.log(msg)
 	}
 }
 
 const error = (msg: string) => {
+	//There should be no logging during test runs
+	//so error logging is ignored from code coverage
+	/* istanbul ignore next */
 	if (NODE_ENV !== 'test') {
-		//There should be no logging during test runs
-		//so error logging is ignored from code coverage
-		/* istanbul ignore next */
 		console.error(msg)
 	}
 }
